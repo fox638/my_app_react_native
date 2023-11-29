4d93ae4e6e1f0264f98524e78781965125c271a9
Dima <fox6383@gmail.com>
2023-11-29 07:59:13 +0200

    add env config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,18 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: false,
+        verbose: false,
+      },
+    ],
+    [
       'module-resolver',
       {
         root: ['./src'],
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,11 @@
+import {API_URL} from '@env';
 import React from 'react';
 import styled from 'styled-components/native';
 
 interface AppProps {}
 
 export const App: React.FC<AppProps> = ({}) => {
-  return <Test>TestMessage</Test>;
+  return <Test>{API_URL}</Test>;
 };
 
 export default App;
