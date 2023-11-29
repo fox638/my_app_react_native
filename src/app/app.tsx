7aa35423998c1550afc6cadae8406326832f1163
Dima <fox6383@gmail.com>
2023-11-29 09:43:25 +0200

    add react navigation

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,22 +1,15 @@
-import {API_URL} from '@env';
 import React from 'react';
-import styled, {css} from 'styled-components/native';
 import AppProviders from './providers';
+import Root from '@screens/Root';
 
 interface AppProps {}
 
 export const App: React.FC<AppProps> = ({}) => {
   return (
     <AppProviders>
-      <Test>{API_URL}</Test>
+      <Root />
     </AppProviders>
   );
 };
 
 export default App;
-
-const Test = styled.Text`
-  ${({theme}) => css`
-    color: ${theme.colors.primaryDark};
-  `}
-`;
